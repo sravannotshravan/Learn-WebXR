ffda101161d1551e7f512259b39e1ff70487878f
sravannotshravan <sravan.kowshik@outlook.com>
2025-07-20 13:44:55 +0530

    Update app.js

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -6,17 +6,35 @@ class App{
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
     
-        
+        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+		this.camera.position.set(0,0,4);
+		
+		this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0xaaaaaa);
+		this.renderer = new THREE.WebGLRenderer({antialias: true});
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		container.appendChild(this.renderer.domElement);
+
+        this.orbitControls = new OrbitControls(this.camera, this.renderer.domElement);
+
+
+        this.renderer.setAnimationLoop(this.render.bind(this));
         window.addEventListener('resize', this.resize.bind(this) );
-	}	
+	}    
     
     resize(){
-        
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
     
 	render( ) {  
-        
+        this.renderer.render(this.scene, this.camera);
     }
 }
 
 export { App };
+
+// At the bottom of app.js or in your main entry file
+const app = new App();
